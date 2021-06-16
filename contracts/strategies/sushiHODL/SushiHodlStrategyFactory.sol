acb9be4a1aff56408e0138ebf701605ce54895f2
lakshmikanth p <lakshmikanthp@lakshmikanths-MacBook-Pro.local>
2021-06-16 22:04:12 +0800

    sushiHODLFactory contract initialise updated and hardhat-tasks added

--- contracts/strategies/sushiHODL/SushiHodlStrategyFactory.sol
+++ contracts/strategies/sushiHODL/SushiHodlStrategyFactory.sol
@@ -7,16 +7,21 @@ contract SushiHodlStrategyFactory {
   address public abc_xyz_unused; // just a differentiator for the bytecode
 
   event LogSushiHodlStrategyCreated(
-      address _storage,
-      address _underlyingPair,
-      address _vault,
-      address _masterChef,
-      address _sushiToken,
-      uint256 _poolId,
-      address _fsushi,
-      address _uniswapRouterV2Address,
-      address _sushiswapRouterV2Address
+    address _storage,
+    address _underlyingPair,
+    address _vault,
+    address _masterChef, // _rewardPool
+    uint256 _poolId,  // Sushi reward pool id
+    address _fsushi //_hodlVault Sushi hodlVault fsushi
+    address _routerAddressV2, //uniswap Router
+    address _sushiTokenAddress, //sushi tokenAddress
+    address _wmaticTokenAddress, //wmatic tokenAddress
+    address[] _routeWmaticToken0, //wmatic->pool-token0 route
+    address[] _routeWmaticToken1, //wmatic->pool-token1 route
+    address[] _routeSushiToken0, //sushi->pool-token0 route
+    address[] _routeSushiToken1 //sushi->pool-token0 route
   );
+
 
   function createSushiHodlStrategy(
     address _storage,
@@ -34,28 +39,36 @@ contract SushiHodlStrategyFactory {
     address[] _routeSushiToken1 //sushi->pool-token0 route
   ) public returns(address) {
     MasterChefHodlStrategy addrNew = new MasterChefHodlStrategy();
-
     MasterChefHodlStrategy(address(addrNew)).initializeMasterChefHodlStrategy(
       _storage,
       _underlyingPair,
       _vault,
       _masterChef,
-      _sushiToken,
       _poolId,
       _fsushi,
-      address(0x0000000000000000000000000000000000000000) // manually set it later,
-      _uniswapRouterV2Address
+      _routerAddressV2,
+      _sushiTokenAddress,
+      _wmaticTokenAddress,
+      _routeWmaticToken0,
+      _routeWmaticToken1,
+      _routeSushiToken0,
+      _routeSushiToken1
     );
+
     emit LogSushiHodlStrategyCreated(
-        _storage,
-        _underlyingPair,
-        _vault,
-        _masterChef,
-        _sushiToken,
-        _poolId,
-        _fsushi,
-        _uniswapRouterV2Address,
-       _sushiswapRouterV2Address
+      _storage,
+      _underlyingPair,
+      _vault,
+      _masterChef,
+      _poolId,
+      _fsushi,
+      _routerAddressV2,
+      _sushiTokenAddress,
+      _wmaticTokenAddress,
+      _routeWmaticToken0,
+      _routeWmaticToken1,
+      _routeSushiToken0,
+      _routeSushiToken1
     );
     return address(addrNew);
   }
---
+++ scripts/deploy/strategy/sushiHODL/deploy-sushiHodl-Storage.task.ts
@@ -0,0 +1,25 @@
+import { logDeployment } from "./deploymentLogUtils";
+import { task, types } from "hardhat/config";
+import "@nomiclabs/hardhat-ethers";
+import { Logger } from "tslog";
+const log: Logger = new Logger();
+
+// npx hardhat compile
+// npx hardhat deploy-storage --network polygonmumbai
+task("deploy-storage", "Deploys a new Storage contract")
+.setAction(
+  async (args, hre) => {
+    log.info("---------== deploy-Storage");
+    const factoryStorage = await hre.ethers.getContractFactory(`contracts/Storage.sol:Storage`);
+    const instStorage = await factoryStorage.deploy();
+    log.info(`--------- Must Do Activity: ${instStorage} gets in to .env as: STORAGE_CONTRACT_ADDRESS --------- `);
+    logDeployment(instStorage, hre.network.name);
+
+    console.log(
+      `npx hardhat verify --contract contracts/Storage.sol:Storage --network ${hre.network.name} ${instStorage.address}`
+    );//npx hardhat verify --contract contracts/Storage.sol:Storage --network ${network} 0x75c8...
+  
+  }
+);
+
+module.exports;
