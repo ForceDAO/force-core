2bd279271aed1944b8c990f5cf823001e63501ec
lakshmikanth p <lakshmikanthp@lakshmikanths-MacBook-Pro.local>
2021-06-19 00:31:30 +0800

    polygonmainnet deployment log and deploy-config with mainnet-addresses of Storage and Vault Contracts

--- scripts/deploy/config/deploy-config.ts
+++ scripts/deploy/config/deploy-config.ts
@@ -1,7 +1,7 @@
 export const deployedContracts = {  
-    network : "polygonmumbai",
-    storageAddress:  "",
-    vaultAddress: ""
+    network : "polygonmainnet",
+    storageAddress:  "0x14d305796672579ee5805c193e4c2A2636c6B61F",
+    vaultAddress: "0xf92C971628828b3d2fb091FD6343DEd5Bb3234Eb"
 }
 
 export default deployedContracts;
