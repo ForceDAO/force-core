739ce49d5af7dd05b9602fa38e0c36fc5aa4ac99
lakshmikanth p <lakshmikanthp@lakshmikanths-MacBook-Pro.local>
2021-06-24 03:14:43 +0800

    deployed and verified sushiHODL Strategy contracts

--- scripts/deploy/strategy/sushiHODL/config/deploy-sushiHodl-USDC-USDT-polygon-mainnet-config.ts
+++ scripts/deploy/strategy/sushiHODL/config/deploy-sushiHodl-USDC-USDT-polygon-mainnet-config.ts
@@ -12,7 +12,7 @@ export const sushiHodlStrategyConfig = {
     routeSushiToken1: ["0x0b3f868e0be5597d5db7feb59e1cadbb0fdda50a", "0xc2132D05D31c914a87C6611C10748AEb04B58e8F"],
     routeWmaticToken0: ["0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270", "0x2791bca1f2de4661ed88a30c99a7a9449aa84174"],
     routeWmaticToken1: ["0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270", "0xc2132D05D31c914a87C6611C10748AEb04B58e8F"],
-    masterChefHodlStrategyAddress: "0x40b14dd05873423f75eea6d0ab6c3a4805518e69",
+    masterChefHodlStrategyAddress: "0x5e6dccb2df12d78c13fb1e3195f05f1afefce5d1",
     storageAddress:  "0x7E428A383D0F3A3B8e2D4a0cA2cDde8792878e2c",
     vaultAddress: "0x8D7f8722B796526B7DBe94055cb405148cc47719",
 }
