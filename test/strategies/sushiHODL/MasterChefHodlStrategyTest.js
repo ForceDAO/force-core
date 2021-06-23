fb5dbc869d635312493981f3ae64f4a0c616f244
lakshmikanth p <lakshmikanthp@lakshmikanths-MacBook-Pro.local>
2021-06-23 21:55:07 +0800

    Fix global test configuration values for polygon-mainnet-fork tests

--- test/strategies/sushiHODL/MasterChefHodlStrategyTest.js
+++ test/strategies/sushiHODL/MasterChefHodlStrategyTest.js
@@ -1,7 +1,7 @@
 const { ethers, upgrades } = require("hardhat");
 const { expect, use } = require("chai");
 const chaiAlmost = require("chai-almost");
-
+const PMFTC = require("../../polygon-mainnet-fork-test-config.js");
 use(chaiAlmost());
 
 const checkAlmostSame = (a, b) => {
@@ -11,19 +11,16 @@ const checkAlmostSame = (a, b) => {
 
 const units = (value) => ethers.utils.parseUnits(value.toString());
 
-const sushiswapV2Router = "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506";
-const sushiswapFactory = "0xc35DADB65012eC5796536bD9864eD8773aBc74C4";
-
 // For mainnet fork tests
-const wmatic = "0x8e07dAfa396B1b2B226367D0266e009cA1B3248d";
-const force = "0x624429a012a8A935cc1110A9880B2d698587a744";
-const sushi = "0x624429a012a8A935cc1110A9880B2d698587a744";
-const usdc = "";
-const usdt = "";
-const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
-const masterChefHodlStrategyAddress = "0x8eA874C0d1cA8C7F55B1FC5A3B0603D43D6a9f9E";
-const strategyOwner = "0x9782968954A2948EB5A611235b7E3D227457DeC0";
-const strategyGovernance = "0x9782968954A2948EB5A611235b7E3D227457DeC0";
+const wmatic = PMFTC.WMATIC_ADDRESS;
+const force = PMFTC.FORCE_ADDRESS;
+const sushi = PMFTC.SUSHI_ADDRESS;
+const usdc = PMFTC.USDC_ADDRESS;
+const usdt = PMFTC.USDT_ADDRESS;
+const ZERO_ADDRESS = PMFTC.ZERO_ADDRESS;
+const masterChefHodlStrategyAddress = PMFTC.MASTER_CHEF_HODL_STRATEGY_ADDRESS;
+const strategyOwner = PMFTC.STRATEGY_OWNER;
+const sushiswapV2Router = PMFTC.SUSHISWAP_V2_ROUTER02_ADDRESS;
 
 describe("MasterChefV2 - USDC_USDT mainnet fork Tests", function () {
     let WMatic, WETH, USDC, USDT;
@@ -36,11 +33,6 @@ describe("MasterChefV2 - USDC_USDT mainnet fork Tests", function () {
       params: [strategyOwner]}
     );
     
-    await hre.network.provider.request({
-      method: "hardhat_impersonateAccount",
-      params: [strategyGovernance]}
-    );
-
   });
 
   it("masterChefHodlStrategy should have all the routes initialised", async () => {
