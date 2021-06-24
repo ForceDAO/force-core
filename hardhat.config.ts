c5d5a17c18c2971bee727587ea5b5f2ce6e07af3
lakshmikanth p <lakshmikanthp@lakshmikanths-MacBook-Pro.local>
2021-06-24 22:54:59 +0800

    deleted obselete contract-tests and tests

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -24,6 +24,7 @@ require("./scripts/verify/strategy/sushiHODL/verify-sushihodl-vault.task.ts");
 require("./scripts/verify/strategy/sushiHODL/verify-sushihodl-strategy.task.ts");
 require("./scripts/verify/verify-storage.task.ts");
 require("./scripts/verify/verify-staticshelper.task.ts");
+require("./scripts/vaults/set-strategy.task.ts");
 
 const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID || "";
 const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY || "";
