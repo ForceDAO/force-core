f659b26087efeb2ae9885b5c2d864b1e695cf70a
lakshmikanth p <lakshmikanthp@lakshmikanths-MacBook-Pro.local>
2021-07-05 15:25:58 +0800

    hardhat script for setting controller to storage

--- scripts/deploy/config/deploy-config-global.ts
+++ scripts/deploy/config/deploy-config-global.ts
@@ -1,5 +1,6 @@
 export const network = "polygonmainnet";
+export const deployerAddress = "0x9782968954A2948EB5A611235b7E3D227457DeC0";
+export const controllerAddress = "0x9782968954A2948EB5A611235b7E3D227457DeC0";
 export const storageAddress = "0x7E428A383D0F3A3B8e2D4a0cA2cDde8792878e2c";
 export const staticsHelperAddress = "0xC1f99f723C7bDF1313140BFA29390138F1b325bf";
 export const sushiHodlStrategyFactoryAddress = "0x156Bf6C58C54237423Ffd7803Cc9303AeaF45ada";
-export const deployerAddress = "0x9782968954A2948EB5A611235b7E3D227457DeC0";
