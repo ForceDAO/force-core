c2b67400f1c56a5181b334ba6712b8fa96810f2b
lakshmikanth p <lakshmikanthp@lakshmikanths-MacBook-Pro.local>
2021-07-06 23:01:57 +0800

    end2end test for sushiHODLStrategy for USDC-USDT underlying on forked mainnet

--- test/strategies/sushiHODL/masterchef-sushihodl-vault.spec.ts
+++ test/strategies/sushiHODL/masterchef-sushihodl-vault.spec.ts
@@ -5,7 +5,9 @@ import { SUSHI_LP_UNDERLYING_ADDRESS_USDC_USDT,
          VAULT_ADDRESS_USDC_USDT,
          STRATEGY_OWNER,
          MASTER_CHEF_HODL_STRATEGY_ADDRESS_USDC_USDT,
-         USDC_ADDRESS, USDC_WHALE_ADDRESS, USDT_ADDRESS, USDT_WHALE_ADDRESS, SUSHISWAP_V2_ROUTER02_ADDRESS } 
+         USDC_ADDRESS, USDC_WHALE_ADDRESS,
+          USDT_ADDRESS, USDT_WHALE_ADDRESS,
+           SUSHISWAP_V2_ROUTER02_ADDRESS } 
          from "../../polygon-mainnet-fork-test-config";
 import { Logger } from "tslog";
 const logger: Logger = new Logger();
@@ -17,6 +19,8 @@ describe("MasterChef V2 - SushiHODL Vault mainnet fork Tests", function () {
     let vaultInstance : any;
     let strategyInstance : any;
     let routerInstance: any;
+    let strategyOwnerSigner: any;
+    let lpBalance: number;
     
     before(async function () {
         [user] = await ethers.getSigners();
@@ -55,16 +59,15 @@ describe("MasterChef V2 - SushiHODL Vault mainnet fork Tests", function () {
         );
 
         const usdtWhaleSigner = await ethers.provider.getSigner(USDT_WHALE_ADDRESS);
-        expect(usdtWhaleSigner).to.not.be.null; 
+        expect(usdtWhaleSigner).to.not.be.null;
 
-        const strategyOwnerSigner = await ethers.provider.getSigner(STRATEGY_OWNER);
+        strategyOwnerSigner = await ethers.provider.getSigner(STRATEGY_OWNER);
         expect(strategyOwnerSigner).to.not.be.null; 
     
         console.log(`impersonated USDT_WHALE_ADDRESS`);
         
         const usdcInstance = await ethers.getContractAt("IERC20", USDC_ADDRESS);
-        console.log(`approving usdc spending`);
-        await usdcInstance.connect(usdcWhaleSigner).approve(STRATEGY_OWNER, 100);
+       await usdcInstance.connect(usdcWhaleSigner).approve(STRATEGY_OWNER, 100);
         console.log(`approved usdc spending`);
 
         const usdcAllowance = await usdcInstance.connect(usdcWhaleSigner).allowance(USDC_WHALE_ADDRESS,STRATEGY_OWNER);
@@ -99,13 +102,14 @@ describe("MasterChef V2 - SushiHODL Vault mainnet fork Tests", function () {
         await routerInstance.connect(strategyOwnerSigner).addLiquidity(USDC_ADDRESS,USDT_ADDRESS, 10, 10, 0, 0, STRATEGY_OWNER, 1925582791);
         console.log(`added USDC-USDT liquidity to SUSHISWAP_V2_ROUTER02_ADDRESS: ${SUSHISWAP_V2_ROUTER02_ADDRESS}`);
 
+        underlyingInstance = await ethers.getContractAt("IERC20", SUSHI_LP_UNDERLYING_ADDRESS_USDC_USDT);    
+        lpBalance = await underlyingInstance.balanceOf(STRATEGY_OWNER);
+        console.log(`lpBalance: ${lpBalance}`);
+
         strategyInstance = await ethers.getContractAt("MasterChefHodlStrategy", MASTER_CHEF_HODL_STRATEGY_ADDRESS_USDC_USDT);
         expect(strategyInstance).to.not.be.null;
-
-        const signer = await ethers.provider.getSigner("0x364d6D0333432C3Ac016Ca832fb8594A8cE43Ca6");
-        expect(signer).to.not.be.null;
-
         vaultInstance = await ethers.getContractAt("Vault", VAULT_ADDRESS_USDC_USDT);
+        expect(vaultInstance).to.not.be.null;
     });
 
     describe("deposit and doHardWork", async () => {
@@ -115,30 +119,47 @@ describe("MasterChef V2 - SushiHODL Vault mainnet fork Tests", function () {
       // then: 
       it("should compound rewards after 1 week", async () => {
 
-        // approve LP Spending for Vault, 
-        // deposit into vault,
-        // vault deposits into strategy
-        // strategy deposits into MiniChef
+            // approve LP Spending for Vault, 
+            // deposit into vault,
+            // vault deposits into strategy
+            // strategy deposits into MiniChef
+
+        //approve for Vault
+        
+        console.log(`approving lpAllowanceForVault: ${VAULT_ADDRESS_USDC_USDT}`);
+        await underlyingInstance.connect(strategyOwnerSigner).approve(VAULT_ADDRESS_USDC_USDT, lpBalance);
+        //await underlyingInstance.connect(strategyOwnerSigner).approve(VAULT_ADDRESS_USDC_USDT, lpBalance);
+        console.log(`approved lpAllowanceForVault: ${VAULT_ADDRESS_USDC_USDT}`);
+
+        const lpAllowanceForVault = await underlyingInstance.connect(strategyOwnerSigner).allowance(STRATEGY_OWNER, VAULT_ADDRESS_USDC_USDT);
+        console.log(`lpAllowanceForVault is: ${lpAllowanceForVault}`);
+
+        //approve vault sample transaction
+        //https://polygonscan.com/tx/0x1a75ff5f5fa046bb3ca71acf2a1ed2406ec03942c2b5528f3881540ce482734a
+    
+        //USDC-USDT pair
+        //https://polygonscan.com/address/0x4b1f1e2435a9c96f7330faea190ef6a7c8d70001
 
 
+        //deposit To Vault
 
-      //approve for Vault
+        //deposit To vault sample transaction
+        // https://polygonscan.com/tx/0x981cb7912d3dc3d0c7dd5894e1bda738423d237c33768e0a9126e68689ff29fb
+        const VaultDeposittxResponse = await vaultInstance.connect(strategyOwnerSigner).deposit(lpBalance);
+        await VaultDeposittxResponse.wait();
+        console.log(`VaultDeposittxResponse is: ${JSON.stringify(VaultDeposittxResponse)}`);  
 
-      //approve vault sample transaction
-      //https://polygonscan.com/tx/0x1a75ff5f5fa046bb3ca71acf2a1ed2406ec03942c2b5528f3881540ce482734a
-  
-      //USDC-USDT pair
-      //https://polygonscan.com/address/0x4b1f1e2435a9c96f7330faea190ef6a7c8d70001
+        await ethers.provider.send("evm_increaseTime", [3600 * 24]);
+        await ethers.provider.send("evm_mine", []);
 
+         const hardWorkTxResponse = await vaultInstance.connect(strategyOwnerSigner).doHardWork();
+         await hardWorkTxResponse.wait();
+         console.log(`hardWorkTxResponse is: ${JSON.stringify(hardWorkTxResponse)}`);  
+ 
+         const lpBalanceAfterHardwork = await underlyingInstance.balanceOf(STRATEGY_OWNER);
+         console.log(`lpBalance After hardwork: ${lpBalanceAfterHardwork}`);
+         expect(lpBalanceAfterHardwork).to.be.equal(0);
 
-      //deposit To Vault
-
-     //deposit To vault sample transaction
-      // https://polygonscan.com/tx/0x981cb7912d3dc3d0c7dd5894e1bda738423d237c33768e0a9126e68689ff29fb
-
-        const depositAmount = 100;
-        const txResponse = await vaultInstance.deposit(depositAmount);
-        const hardWorkTxResponse = await vaultInstance.doHardWork();
       });
 
     });
