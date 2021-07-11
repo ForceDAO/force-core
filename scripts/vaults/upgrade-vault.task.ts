612747c82c960c594ff44c21336baec52fd4f0e0
lakshmikanth p <lakshmikanthp@lakshmikanths-MacBook-Pro.local>
2021-07-12 02:59:56 +0800

    extract implementationAddress from EventLogs of vault's ProxyUpgrade transaction

--- scripts/vaults/upgrade-vault.task.ts
+++ scripts/vaults/upgrade-vault.task.ts
@@ -2,9 +2,10 @@ import { task } from "hardhat/config";
 import { Logger } from "tslog";
 import "@nomiclabs/hardhat-ethers";
 const log: Logger = new Logger();
-import { ethers } from "ethers";
 import { strict as assert } from 'assert';
+import { ethers, network } from "hardhat";
 import { vaults, Vault }  from "../deploy/config/deploy-config-vaults";
+import { logger } from "ethers";
 
 task("upgrade-vault", "set the totalSupplyCap to the Vault")
 .addParam("underlyingname","name of the underlying, for Example: USDC-USDT")
@@ -13,7 +14,7 @@ task("upgrade-vault", "set the totalSupplyCap to the Vault")
     const underlyingname : string = args.underlyingname;
     const vault : Vault = vaults[underlyingname];
     const vaultAddress : string = vault.vaultAddress;
-    const vaultImplementationAddress: string = vault.vaultImplementationAddress;
+    const vaultImplementationAddress : string = vault.vaultImplementationAddress;
     assert(ethers.utils.getAddress(vaultAddress) == ethers.utils.getAddress(vaultAddress), "Invalid vaultAddress");
     assert(ethers.utils.getAddress(vaultImplementationAddress) == ethers.utils.getAddress(vaultImplementationAddress), "Invalid vaultAddress");
     log.info(`vaultAddress : ${vaultAddress} with implementation: ${vaultImplementationAddress} for underlying: ${underlyingname}`);
@@ -26,4 +27,27 @@ task("upgrade-vault", "set the totalSupplyCap to the Vault")
     await vaultUpgradeTxnResponse.wait();
 
     log.info(`transactionResponse for  VaultProxy: ${vaultAddress} is: ${JSON.stringify(vaultUpgradeTxnResponse)}`);
+    assert(vaultUpgradeTxnResponse.status == 0, "vaultUpgradeFailed");
+
+    const implementationAddressTopicHash =  ethers.utils.keccak256(ethers.utils.solidityPack(["string"], ["Upgraded(address)"]));
+
+    var newImplementationAddress;
+
+    //lookup for implementationAddress in EventLog which has topic-0 matching implementationAddressTopicHash
+    const transactionReceipt = await ethers.provider.getTransactionReceipt(vaultUpgradeTxnResponse.hash);
+
+    const logs = transactionReceipt.logs;
+    const logIndex = logs.length;
+
+    for(let i =  0 ; i < logIndex; i++){
+     
+        let log = logs[i];
+
+        if(log.topics[0] == implementationAddressTopicHash){
+            newImplementationAddress = log.topics[1];
+            break;
+        }
+    }
+    
+    logger.info(`Vault: ${vaultAddress} is Upgraded to new-implementationAddress: ${newImplementationAddress}`);
 });
